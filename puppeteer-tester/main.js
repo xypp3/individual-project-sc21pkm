246d19e8ec9dc2b35830ee51b2e637cf3b9bc72d
xypp3 <a@b.com>
2024-04-22 04:28:51 +0100

    feat: Bitrate caputred by regexp from url && Wait on #hasEnded before browser.close()

--- puppeteer-tester/main.js
+++ puppeteer-tester/main.js
@@ -1,6 +1,7 @@
 import puppeteer from 'puppeteer';
 
 const bitrateConv = new Map();
+bitrateConv.set('', '');
 
 (async () => {
 	let url = 'http://127.0.0.1:5173';
@@ -17,20 +18,17 @@ const bitrateConv = new Map();
 	await page.click("button");
 
 	// intercept requests and apply bandwidth
-	// TODO: 1. Find bandwidth calculation
+	// TODO: 1x. Find bandwidth calculation
 	// 2. Make value programmable
-	// 3. Make it variable (ala zipf function)
-	let interceptBandwidth = 700;
+	// 3. Make it variable (dash test suite in bola study) (ala zipf function)
+	let interceptBandwidth = 9000; // usually in Mbi but bitrate in Kbi
 	await page.setRequestInterception(true);
 	page.on('request', interceptedRequest => {
 		if (interceptedRequest.isInterceptResolutionHandled()) return;
 		const reqUrl = interceptedRequest.url();
 		if (reqUrl.endsWith(".m4v")) {
 			// map req url to birate
-			console.log(reqUrl);
-			console.log(reqUrl.match("_([0-9]*)k_")[1])
-			const bitrate = bitrateConv.get(reqUrl.match("_([0-9]*)k_")[1]);
-			console.log(bitrate);
+			const bitrate = reqUrl.match("_([0-9]*)k_")[1];
 			const timeDelay = bitrate / interceptBandwidth;
 			setTimeout(async () => { await interceptedRequest.continue() }, timeDelay);
 		} else {
@@ -38,7 +36,7 @@ const bitrateConv = new Map();
 		}
 	});
 
-	// TODO: make this propah
-	setTimeout(async () => { await browser.close() }, 10000);
+	await page.waitForFunction(() => { return document.querySelector("#hasEnded").innerHTML !== "false" });
+	await browser.close();
 
 })();
